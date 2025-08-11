7997444c12afb9ce6d296b133c937f8a34ebb3fe
Aaditya Bhatia <aaditya.tec@gmail.com>
2025-08-11 14:49:23 +0530

    Update

--- INS Lab/1.cpp
+++ INS Lab/1.cpp
@@ -1,32 +1,45 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-string encrypt(string text, int s)
-{
+string encrypt(string text, int s) {
     string result = "";
 
     for (int i = 0; i < text.length(); i++) {
-
-        if (isupper(text[i]))
-            result += char(int(text[i] + s - 65) % 26 + 65);
-
-        else
-            result += char(int(text[i] + s - 97) % 26 + 97);
+        if (isupper(text[i])) {
+            result += char(int(text[i] - 'A' + s) % 26 + 'A');
+        }
+        else if (islower(text[i])) {
+            result += char(int(text[i] - 'a' + s) % 26 + 'a');
+        }
+        else {
+            result += text[i]; // Keep non-alphabetic characters unchanged
+        }
     }
     return result;
 }
 
-
-
-int main()
-{
-    string text;
-    int s;
-    
-    cout << "Text : " << text;
-    cout << "\nShift: " << s;
-    cout << "\nCipher: " << encrypt(text, s);
-    return 0;
+string decrypt(string text, int s) {
+    return encrypt(text, 26 - (s % 26));     // Decrypt by shifting backwards
 }
 
-// By Aaditya Bhatia
+int main() {
+    string text;
+    int s;
+
+    cout << "Enter text: ";
+    getline(cin, text);
+
+    cout << "Enter shift value: ";
+    cin >> s;
+
+    string encrypted = encrypt(text, s);
+    string decrypted = decrypt(encrypted, s);
+
+    cout << "\nOriginal : " << text;
+    cout << "\nShift    : " << s;
+    cout << "\nEncrypted: " << encrypted;
+    cout << "\nDecrypted: " << decrypted << endl;
+
+    return 0;
+}
